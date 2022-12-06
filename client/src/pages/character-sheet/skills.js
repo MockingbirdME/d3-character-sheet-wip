3788ac668e0ba76fc887143ccb535fb10165e823
Devon-Henegar <devon.henegar@ibm.com>
2022-12-06 16:10:19 -0500

    Update Simu and character sheet

--- client/src/pages/character-sheet/skills.js
+++ client/src/pages/character-sheet/skills.js
@@ -1,22 +1,50 @@
+import {useState} from "react";
+
 import CharacterSheetSkill from "./skill";
 
 function CharacterSheetSkills(params) {
+
+
+  const [displayAllTraits, setDisplayAllTraits] = useState(false);
+
+  const toggleTraitDisplay = () => {
+    console.log('toggle');
+    setDisplayAllTraits(!displayAllTraits)
+  }
+
   const {characterData} = params;
-  console.log(characterData);
 
   if (!characterData.resources) return (<div></div>)
-
-  // const skills = ['animalHandling', 'awareness', 'legerdemain', 'legerdemain'];
 
   const skills = Object.keys(characterData.skills)
 
   const skillsDisplay = skills.map(skill => (
-    <CharacterSheetSkill skillData={characterData.skills[skill]} />
+    <CharacterSheetSkill 
+      skillData={characterData.skills[skill]} 
+      displayAllTraits={displayAllTraits}
+    />
   ))
+
+  const showAllTraitsButton = (
+    <div 
+      className="character_sheet__toggle_button"
+      onClick={toggleTraitDisplay}
+    >Show All Traits</div>
+  )
+
+  const hideSomeTraitsButton = (
+    <div 
+      className="character_sheet__toggle_button"
+      onClick={toggleTraitDisplay}
+    >Show All Traits</div>
+  )
 
   return (
     <div className="character_sheet__skills">
-      <b style={{textDecoration: 'underline'}} >Skills</b>
+      <div className="character_sheet__skills__header_row">
+        <b style={{textDecoration: 'underline'}}>Skills</b>
+        {displayAllTraits ? showAllTraitsButton : hideSomeTraitsButton}
+      </div>
       <div className="character_sheet__resources_groups">
         {skillsDisplay}
       </div>
